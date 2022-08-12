5e73ccc250bf00bc696b2e3aee925c2aa769247c
Rumi <moonding901212@gmail.com>
2022-08-12 17:34:13 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Form from './components/Form';
 
 function App() {
+    const jsonLocalStorage = {
+        setItem: (key, value) => {
+            localStorage.setItem(key, JSON.stringify(value));
+        },
+        getItem: (key) => {
+            return JSON.parse(localStorage.getItem(key));
+        },
+    };
+    const Title = (props) => {
+        return (
+            <div>
+                <h1>{props.children}</h1>
+            </div>
+        )
+    }
+
+    // const Form = ({ updateMainCat }) => {
+    //     const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
+    //     const [value, setValue] = useState('')
+    //     const [errorMessage, setErrorMessage] = useState('')
+    //
+    //     const handleInputChange = (e) => {
+    //         const userValue = e.target.value;
+    //         setErrorMessage('')
+    //         if (includesHangul(userValue)) {
+    //             setErrorMessage('한글은 입력할 수 없습니다.')
+    //         }
+    //         setValue(userValue.toUpperCase())
+    //     }
+    //     const handleFormSubmit = (e) => {
+    //         e.preventDefault();
+    //         setErrorMessage('')
+    //
+    //         if (value === '') {
+    //             setErrorMessage('빈 값으로 만들 수 없습니다.')
+    //             return
+    //         }
+    //         updateMainCat();
+    //     }
+    //
+    //     return (
+    //         <form onSubmit={handleFormSubmit}>
+    //             <input type="text" name="name" placeholder="영어 대사를 입력해주세요" value={value} onChange={handleInputChange} />
+    //             <button type="submit">생성</button>
+    //             <p style={{ color: 'red' }}>{errorMessage}</p>
+    //         </form>
+    //     )
+    // }
+
+    function CatItem(props) {
+        return (
+            <li>
+                <img src={props.img} style={{ width: "150px" }} />
+            </li>
+        )
+    }
+    function Favorites({ favorites }) {
+
+        return (
+            <ul className="favorites">
+                {favorites.map((cat) => (
+                    <CatItem key={cat} img={cat} />
+                ))}
+            </ul>
+        )
+    }
+    const MainCard = ({ img, onHeartClick }) => {
+
+        return (
+            <div className="main-card">
+                <img src={img} alt="고양이" width="400" />
+                <button onClick={onHeartClick}>🤍</button>
+            </div>
+        )
+    }
+    const CAT1 = "https://cataas.com/cat/60b73094e04e18001194a309/says/react";
+    const CAT2 = "https://cataas.com//cat/5e9970351b7a400011744233/says/inflearn";
+    const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
+    const [counter, setCounter] = useState(jsonLocalStorage.getItem("counter"));
+    //localStorage로 가져온 값이 string값으로 가져와지기 떄문에 number로 바꿔야 counter값이 제대로 잘 더해진다.
+    const [mainCat, setMainCat] = useState(CAT1);
+    const [favorites, setFavorites] = useState(jsonLocalStorage.getItem("favorites") || [])
+
+    const updateMainCat = () => {
+        setMainCat(CAT2)
+        const nextCounter = counter + 1
+        setCounter(nextCounter)
+        jsonLocalStorage.setItem("counter", nextCounter)
+    }
+    function handleHeartClick() {
+        const nextFavorites = [...favorites, mainCat]
+        setFavorites(nextFavorites)
+        jsonLocalStorage.setItem('favorites', nextFavorites)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Title>{counter}번째 고양이 가라사대</Title>
+        <Form updateMainCat={updateMainCat} />
+        <MainCard img={mainCat} onHeartClick={handleHeartClick} />
+        <Favorites favorites={favorites} />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {useState} from "react";
+
+const Form = ({ updateMainCat }) => {
+    const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
+    const [value, setValue] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleInputChange = (e) => {
+        const userValue = e.target.value;
+        setErrorMessage('')
+        if (includesHangul(userValue)) {
+            setErrorMessage('한글은 입력할 수 없습니다.')
+        }
+        setValue(userValue.toUpperCase())
+    }
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        setErrorMessage('')
+
+        if (value === '') {
+            setErrorMessage('빈 값으로 만들 수 없습니다.')
+            return
+        }
+        updateMainCat();
+    }
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <input type="text" name="name" placeholder="영어 대사를 입력해주세요" value={value} onChange={handleInputChange} />
+            <button type="submit">생성</button>
+            <p style={{ color: 'red' }}>{errorMessage}</p>
+        </form>
+    )
+}
+
+export default Form;
